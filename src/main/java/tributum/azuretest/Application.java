11e1bb652210848d17a14959545452add4201015
Christoph von Babo <cvbabo@web.de>
2020-01-17 18:22:10 +0100

    n

--- src/main/java/tributum/azuretest/Application.java
+++ src/main/java/tributum/azuretest/Application.java
@@ -12,6 +12,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jms.core.JmsTemplate;
 
+import com.microsoft.azure.storage.CloudStorageAccount;
+import com.microsoft.azure.storage.queue.CloudQueue;
+import com.microsoft.azure.storage.queue.CloudQueueClient;
+import com.microsoft.azure.storage.queue.CloudQueueMessage;
+
+import tributum.azuretest.service.TaxMessageService;
+
 @SpringBootApplication
 public class Application implements ApplicationRunner {
 
@@ -22,7 +29,10 @@ public class Application implements ApplicationRunner {
 	private String outputQueue;
 	
 	@Autowired
-	private JmsTemplate template;
+	private FileHandler fileHandler;
+	
+	@Autowired
+	private TaxMessageService tmsgService;
 	
 	private static final Logger log = LoggerFactory.getLogger(Application.class);
 	
@@ -32,18 +42,38 @@ public class Application implements ApplicationRunner {
 	
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
-		/*try {
-			String txmsg = "<xml><txId>50214235</txId><bPrice>100</bPrice><sPrice>120</sPrice><nos>20</nos><taxRate>25</taxRate></xml>";
-			template.convertAndSend(inputQueue, txmsg);
-			
-			template.setReceiveTimeout(1000);
-			
-			Message m = template.receive(outputQueue);
-			log.info(m.toString());
-			log.info("Published " + ((ActiveMQTextMessage) m).getText() + " to output " + m.getJMSDestination());
-		} catch(Exception e) {
-			log.error(e.toString());
-		}*/
+		final String storageConnectionString =
+			    "DefaultEndpointsProtocol=https;" +
+			    "AccountName=queuestoragetributum;" +
+			    "AccountKey=3lOqCDk9IXhiUDXthOGeuTDli19EZ32/xkAHF4QX8xNxDxHCxbBKZMnkwvp5GlOtC2iLp6yXXlBMYMjr9zdb0A==";
+		
+		try {
+			CloudStorageAccount storageAccount =
+				       CloudStorageAccount.parse(storageConnectionString);
+			CloudQueueClient queueClient = storageAccount.createCloudQueueClient();
+			CloudQueue inputQueue = queueClient.getQueueReference("${input.queue}");
+			CloudQueueMessage inputMsg = inputQueue.peekMessage();
+			if(inputMsg != null) {
+				String inputMsgStr = inputMsg.getMessageContentAsString();
+				log.info("<<< LOGGING STARTS HERE >>>");
+				log.info("Received Message: -- " + inputMsgStr + " -- ");
+				log.info("<<< Message gets processed >>>");
+				double bPrice = Double.parseDouble(fileHandler.getDataFromXMLFormat(inputMsgStr, "bPrice"));
+				double sPrice = Double.parseDouble(fileHandler.getDataFromXMLFormat(inputMsgStr, "sPrice"));
+				int nos = Integer.parseInt(fileHandler.getDataFromXMLFormat(inputMsgStr, "nos"));
+				double taxRate = Double.parseDouble(fileHandler.getDataFromXMLFormat(inputMsgStr, "taxRate"));
+				double result = tmsgService.taxAmount(bPrice, sPrice, nos, taxRate);
+				log.info("Message processed -- Tax amount is: " + String.valueOf(result));
+				String strResult = fileHandler.generateXMLFormat(inputMsgStr, result);
+				CloudQueueMessage outputMsg = new CloudQueueMessage(strResult);
+				CloudQueue outputQueue = queueClient.getQueueReference("${output.queue}");
+				outputQueue.addMessage(outputMsg);
+				log.info("Message processed to queue ${output.queue}");
+				log.info("<<< LOGGING ENDS HERE >>>");
+			}
+		} catch (Exception e) {
+		    log.error(e.toString());
+		}
 	}
 	
 }
--- src/main/java/tributum/azuretest/Consumer.java
+++ src/main/java/tributum/azuretest/Consumer.java
@@ -1,4 +1,4 @@
-package tributum.azuretest;
+/*package tributum.azuretest;
 
 import javax.jms.JMSException;
 
@@ -28,36 +28,7 @@ private static final Logger log = LoggerFactory.getLogger(Consumer.class);
 	private FileHandler fileHandler;
 	
 	public void receiveMessage() throws JMSException {
-		final String storageConnectionString =
-			    "DefaultEndpointsProtocol=https;" +
-			    "AccountName=queuestoragetributum;" +
-			    "AccountKey=3lOqCDk9IXhiUDXthOGeuTDli19EZ32/xkAHF4QX8xNxDxHCxbBKZMnkwvp5GlOtC2iLp6yXXlBMYMjr9zdb0A==";
 		
-		try {
-			CloudStorageAccount storageAccount =
-				       CloudStorageAccount.parse(storageConnectionString);
-			CloudQueueClient queueClient = storageAccount.createCloudQueueClient();
-			CloudQueue inputQueue = queueClient.getQueueReference("${input.queue}");
-			CloudQueueMessage inputMsg = inputQueue.retrieveMessage();
-			String inputMsgStr = inputMsg.toString(); 
-			log.info("<<< LOGGING STARTS HERE >>>");
-			log.info("Received Message: -- " + inputMsgStr + " -- ");
-			log.info("<<< Message gets processed >>>");
-			double bPrice = Double.parseDouble(fileHandler.getDataFromXMLFormat(inputMsgStr, "bPrice"));
-			double sPrice = Double.parseDouble(fileHandler.getDataFromXMLFormat(inputMsgStr, "sPrice"));
-			int nos = Integer.parseInt(fileHandler.getDataFromXMLFormat(inputMsgStr, "nos"));
-			double taxRate = Double.parseDouble(fileHandler.getDataFromXMLFormat(inputMsgStr, "taxRate"));
-			double result = tmsgService.taxAmount(bPrice, sPrice, nos, taxRate);
-			log.info("Message processed -- Tax amount is: " + String.valueOf(result));
-			String strResult = fileHandler.generateXMLFormat(inputMsgStr, result);
-			CloudQueueMessage outputMsg = new CloudQueueMessage(strResult);
-			CloudQueue outputQueue = queueClient.getQueueReference("${output.queue}");
-			outputQueue.addMessage(outputMsg);
-			log.info("Message processed to queue ${output.queue}");
-			log.info("<<< LOGGING ENDS HERE >>>");
-		} catch (Exception e) {
-		    log.error(e.toString());
-		}
 		
 		
 
@@ -67,4 +38,4 @@ private static final Logger log = LoggerFactory.getLogger(Consumer.class);
 		//return resultXMLString;
 	}
 	
-}
+}*/
